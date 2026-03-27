74b06fe50c3a9a9afe85ee54382e6774c997d1ea
Antonio Mendoza Pérez <temporalio@Antonio-Temporalio.local>
2026-03-27 10:27:42 +0100

    initial_structure

---
+++ src/main/java/io/temporal/workflow/hello/HelloWorldWorkflowImpl.java
@@ -0,0 +1,24 @@
+package io.temporal.workflow.hello;
+
+import io.temporal.activity.ActivityOptions;
+import io.temporal.spring.boot.WorkflowImpl;
+import io.temporal.workflow.Workflow;
+
+import java.time.Duration;
+
+
+@WorkflowImpl(taskQueues = "HelloSampleTaskQueue")
+public class HelloWorldWorkflowImpl implements HelloWorldWorkflow {
+
+    private final HelloActivity helloActivity = Workflow.newActivityStub(
+            HelloActivity.class,
+            ActivityOptions.newBuilder()
+                    .setStartToCloseTimeout(Duration.ofSeconds(10))
+                    .build()
+    );
+
+    @Override
+    public String sayHello(String name) {
+        return helloActivity.greet(name);
+    }
+}
---
+++ src/test/java/io/temporal/workflow/hello/HelloWorldWorkflowTest.java
@@ -0,0 +1,51 @@
+package io.temporal.workflow.hello;
+
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.testing.TestWorkflowEnvironment;
+import io.temporal.worker.Worker;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldWorkflowTest {
+
+    private static final String TASK_QUEUE = "HelloSampleTaskQueue";
+
+    private TestWorkflowEnvironment testEnv;
+    private WorkflowClient client;
+
+    @BeforeEach
+    void setUp() {
+        testEnv = TestWorkflowEnvironment.newInstance();
+        client = testEnv.getWorkflowClient();
+
+        Worker worker = testEnv.newWorker(TASK_QUEUE);
+        worker.registerWorkflowImplementationTypes(HelloWorldWorkflowImpl.class);
+        worker.registerActivitiesImplementations((HelloActivity) name -> "Hello " + name + " from mocked activity");
+
+        testEnv.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testEnv.close();
+    }
+
+    @Test
+    void sayHello_returnsGreeting() {
+        HelloWorldWorkflow workflow = client.newWorkflowStub(
+                HelloWorldWorkflow.class,
+                WorkflowOptions.newBuilder()
+                        .setTaskQueue(TASK_QUEUE)
+                        .build()
+        );
+
+        String result = workflow.sayHello("World");
+
+        assertEquals("Hello World from mocked activity", result);
+    }
+}
